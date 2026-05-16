fb28866a254872a45825b62909549cac9edda7ff
nihilne <85189523+nihilne@users.noreply.github.com>
2026-05-17 02:40:31 +0300

    feat: basic hello page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return new Response('<h1>Hello!!</h1>');
+        return $this->render('home.html.twig');
     }
 }
